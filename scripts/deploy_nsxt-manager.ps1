2e1c2fb03d2969d7ce9919e4a9af59e785e1f9ec
vmoimoi <n.n.notion@gmail.com>
2022-01-04 18:08:27 +0900

    change data

--- scripts/deploy_nsxt-manager.ps1
+++ scripts/deploy_nsxt-manager.ps1
@@ -1,5 +1,5 @@
 ﻿#Credentials
-$vcenter="172.31.0.20"
+$vcenter="172.16.xx.xx"
 $vcsa_user="administrator@vsphere.local"
 $vcsa_password="VMware1!"
 
@@ -7,32 +7,32 @@ $vcsa_password="VMware1!"
 Connect-VIServer $vcenter -User $vcsa_user -Password $vcsa_password –force
 
 #Setting
-$ova_file = "C:\work\ISO_OVA\nsx-unified-appliance-3.2.0.0.0.19067081.ova"
+$ova_file = "C:\work\xxxxx.ova"
 $ovf_config = Get-OvfConfiguration $ova_file
 
 #Config Parameter
 $ovf_config.DeploymentOption.Value = "small" #Size
-$ovf_config.NetworkMapping.Network_1.Value = "pg-mgmt" # Portgroup
+$ovf_config.NetworkMapping.Network_1.Value = "pg-xxxx" # Portgroup
 $ovf_config.Common.nsx_hostname.Value = "nsxt-mgr01" # Hostname
 $ovf_config.IpAssignment.IpProtocol.Value = "IPv4" # Ip protocol
-$ovf_config.Common.nsx_ip_0.Value ="172.31.0.22" # Mgmt ip
+$ovf_config.Common.nsx_ip_0.Value ="172.16.xx.xx" # Mgmt ip
 $ovf_config.Common.nsx_netmask_0.Value ="255.255.255.0" # Netmask
-$ovf_config.Common.nsx_gateway_0.Value ="172.31.0.1" # Gateway
+$ovf_config.Common.nsx_gateway_0.Value ="172.16.xx.xx" # Gateway
 $ovf_config.Common.nsx_passwd_0.Value = "VMware1!VMware1!" # Root User Password
 $ovf_config.Common.nsx_cli_passwd_0.Value = "VMware1!VMware1!" # Admin User Password
 $ovf_config.Common.nsx_cli_audit_passwd_0.Value = "VMware1!VMware1!" # Audit User Password
 $ovf_config.Common.nsx_role.Value = "NSX Manager" # Role
-$ovf_config.Common.nsx_dns1_0.Value ="172.31.0.253" # DNS
-$ovf_config.Common.nsx_domain_0.Value = "ad.hlab.com" # Domain search list
-$ovf_config.Common.nsx_ntp_0.Value = "172.31.0.23" # NTP
+$ovf_config.Common.nsx_dns1_0.Value ="172.16.xx.xx" # DNS
+$ovf_config.Common.nsx_domain_0.Value = "xxxx" # Domain search list
+$ovf_config.Common.nsx_ntp_0.Value = "172.16.xx.xx" # NTP
 $ovf_config.Common.nsx_isSSHEnabled.Value = $true  # SSH
 $ovf_config.Common.nsx_allowSSHRootLogin.Value = $true # Root SSH
 
 #Deploy Parameter
 $vm_name = "nsxt-mgr01"
-$host_name = "deskmini-a300-01.ad.hlab.com"
+$host_name = "xxxxx"
 $vm_host = (Get-VMHost -Name $host_name)
-$datastore = "localds-01"
+$datastore = "xxxxx"
 
 #Deploy
 $vm_host | Import-VApp -Source $ova_file -Name $vm_name -Datastore $datastore -DiskStorageFormat "Thin" -OvfConfiguration $ovf_config -Force
